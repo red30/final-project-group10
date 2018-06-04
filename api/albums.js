dba9467b9be4e8a2934c1cd810276c9e03fc4e46
Joshua Lioy <joshua.lioy@icloud.com>
2018-06-04 12:19:26 -0700

    Adjusted the albumSchema.

--- api/albums.js
+++ api/albums.js
@@ -3,29 +3,22 @@ const validation = require('../lib/validation');
 const { getPhotosByAlbumID } = require('./photos');
 
 /*
- * Schema describing required/optional fields of a business object.
+ * Schema describing required/optional fields of a album object.
  */
-const businessSchema = {
+const albumSchema = {
   ownerid: { required: true },
   name: { required: true },
-  address: { required: true },
-  city: { required: true },
-  state: { required: true },
-  zip: { required: true },
-  phone: { required: true },
-  category: { required: true },
-  subcategory: { required: true },
-  website: { required: false },
+  date: { required: true },
   email: { required: false }
 };
 
 /*
- * Executes a MySQL query to fetch the total number of businesses.  Returns
+ * Executes a MySQL query to fetch the total number of albums.  Returns
  * a Promise that resolves to this count.
  */
-function getBusinessesCount(mysqlPool) {
+function getAlbumsCount(mysqlPool) {
   return new Promise((resolve, reject) => {
-    mysqlPool.query('SELECT COUNT(*) AS count FROM businesses', function (err, results) {
+    mysqlPool.query('SELECT COUNT(*) AS count FROM albums', function (err, results) {
       if (err) {
         reject(err);
       } else {
@@ -36,10 +29,10 @@ function getBusinessesCount(mysqlPool) {
 }
 
 /*
- * Executes a MySQL query to return a single page of businesses.  Returns a
- * Promise that resolves to an array containing the fetched page of businesses.
+ * Executes a MySQL query to return a single page of albums.  Returns a
+ * Promise that resolves to an array containing the fetched page of albums.
  */
-function getBusinessesPage(page, totalCount, mysqlPool) {
+function getAlbumsPage(page, totalCount, mysqlPool) {
   return new Promise((resolve, reject) => {
     /*
      * Compute last page number and make sure page is within allowed bounds.
@@ -52,14 +45,14 @@ function getBusinessesPage(page, totalCount, mysqlPool) {
     const offset = (page - 1) * numPerPage;
 
     mysqlPool.query(
-      'SELECT * FROM businesses ORDER BY id LIMIT ?,?',
+      'SELECT * FROM albums ORDER BY id LIMIT ?,?',
       [ offset, numPerPage ],
       function (err, results) {
         if (err) {
           reject(err);
         } else {
           resolve({
-            businesses: results,
+            albums: results,
             pageNumber: page,
             totalPages: lastPage,
             pageSize: numPerPage,
@@ -72,49 +65,49 @@ function getBusinessesPage(page, totalCount, mysqlPool) {
 }
 
 /*
- * Route to return a paginated list of businesses.
+ * Route to return a paginated list of albums.
  */
 router.get('/', function (req, res) {
   const mysqlPool = req.app.locals.mysqlPool;
-  getBusinessesCount(mysqlPool)
+  getAlbumsCount(mysqlPool)
     .then((count) => {
-      return getBusinessesPage(parseInt(req.query.page) || 1, count, mysqlPool);
+      return getAlbumsPage(parseInt(req.query.page) || 1, count, mysqlPool);
     })
-    .then((businessesPageInfo) => {
+    .then((albumsPageInfo) => {
       /*
        * Generate HATEOAS links for surrounding pages and then send response.
        */
-      businessesPageInfo.links = {};
-      let { links, pageNumber, totalPages } = businessesPageInfo;
+      albumsPageInfo.links = {};
+      let { links, pageNumber, totalPages } = albumsPageInfo;
       if (pageNumber < totalPages) {
-        links.nextPage = `/businesses?page=${pageNumber + 1}`;
-        links.lastPage = `/businesses?page=${totalPages}`;
+        links.nextPage = `/albums?page=${pageNumber + 1}`;
+        links.lastPage = `/albums?page=${totalPages}`;
       }
       if (pageNumber > 1) {
-        links.prevPage = `/businesses?page=${pageNumber - 1}`;
-        links.firstPage = '/businesses?page=1';
+        links.prevPage = `/albums?page=${pageNumber - 1}`;
+        links.firstPage = '/albums?page=1';
       }
-      res.status(200).json(businessesPageInfo);
+      res.status(200).json(albumsPageInfo);
     })
     .catch((err) => {
       console.log(err);
       res.status(500).json({
-        error: "Error fetching businesses list.  Please try again later."
+        error: "Error fetching albums list.  Please try again later."
       });
     });
 });
 
 /*
- * Executes a MySQL query to insert a new business into the database.  Returns
- * a Promise that resolves to the ID of the newly-created business entry.
+ * Executes a MySQL query to insert a new album into the database.  Returns
+ * a Promise that resolves to the ID of the newly-created album entry.
  */
-function insertNewBusiness(business, mysqlPool) {
+function insertNewAlbum(album, mysqlPool) {
   return new Promise((resolve, reject) => {
-    business = validation.extractValidFields(business, businessSchema);
-    business.id = null;
+    album = validation.extractValidFields(album, albumSchema);
+    album.id = null;
     mysqlPool.query(
-      'INSERT INTO businesses SET ?',
-      business,
+      'INSERT INTO albums SET ?',
+      album,
       function (err, result) {
         if (err) {
           reject(err);
@@ -127,72 +120,72 @@ function insertNewBusiness(business, mysqlPool) {
 }
 
 /*
- * Route to create a new business.
+ * Route to create a new album.
  */
 router.post('/', function (req, res, next) {
   const mysqlPool = req.app.locals.mysqlPool;
-  if (validation.validateAgainstSchema(req.body, businessSchema)) {
-    insertNewBusiness(req.body, mysqlPool)
+  if (validation.validateAgainstSchema(req.body, albumSchema)) {
+    insertNewAlbum(req.body, mysqlPool)
       .then((id) => {
         res.status(201).json({
           id: id,
           links: {
-            business: `/businesss/${id}`
+            album: `/albums/${id}`
           }
         });
       })
       .catch((err) => {
         res.status(500).json({
-          error: "Error inserting business into DB.  Please try again later."
+          error: "Error inserting album into DB.  Please try again later."
         });
       });
   } else {
     res.status(400).json({
-      error: "Request body is not a valid business object."
+      error: "Request body is not a valid album object."
     });
   }
 });
 
 /*
  * Executes a MySQL query to fetch information about a single specified
- * business based on its ID.  Returns a Promise that resolves to an object
- * containing information about the requested business.  If no business with
+ * album based on its ID.  Returns a Promise that resolves to an object
+ * containing information about the requested album.  If no album with
  * the specified ID exists, the returned Promise will resolve to null.
  */
-function getBusinessByID(businessID, mysqlPool) {
+function getAlbumByID(albumID, mysqlPool) {
   /*
    * Execute three sequential queries to get all of the info about the
-   * specified business, including its reviews and photos.  If the original
-   * request to fetch the business doesn't match a business, send null through
+   * specified album, including its reviews and photos.  If the original
+   * request to fetch the album doesn't match a album, send null through
    * the promise chain.
    */
-  let returnBusiness = {};
+  let returnAlbum = {};
   return new Promise((resolve, reject) => {
-    mysqlPool.query('SELECT * FROM businesses WHERE id = ?', [ businessID ], function (err, results) {
+    mysqlPool.query('SELECT * FROM albums WHERE id = ?', [ albumID ], function (err, results) {
       if (err) {
         reject(err);
       } else {
         resolve(results[0]);
       }
     });
-  }).then((business) => {
-    if (business) {
-      returnBusiness = business;
-      return getReviewsByBusinessID(businessID, mysqlPool);
+  }).then((album) => {
+    if (album) {
+      returnAlbum = album;
+      return getReviewsByAlbumID(albumID, mysqlPool);
     } else {
       return Promise.resolve(null);
     }
   }).then((reviews) => {
     if (reviews) {
-      returnBusiness.reviews = reviews;
-      return getPhotosByBusinessID(businessID, mysqlPool);
+      returnAlbum.reviews = reviews;
+      return getPhotosByAlbumID(albumID, mysqlPool);
     } else {
       return Promise.resolve(null);
     }
   }).then((photos) => {
     if (photos) {
-      returnBusiness.photos = photos;
-      return Promise.resolve(returnBusiness);
+      returnAlbum.photos = photos;
+      return Promise.resolve(returnAlbum);
     } else {
       return Promise.resolve(null);
     }
@@ -200,35 +193,35 @@ function getBusinessByID(businessID, mysqlPool) {
 }
 
 /*
- * Route to fetch info about a specific business.
+ * Route to fetch info about a specific album.
  */
-router.get('/:businessID', function (req, res, next) {
+router.get('/:albumID', function (req, res, next) {
   const mysqlPool = req.app.locals.mysqlPool;
-  const businessID = parseInt(req.params.businessID);
-  getBusinessByID(businessID, mysqlPool)
-    .then((business) => {
-      if (business) {
-        res.status(200).json(business);
+  const albumID = parseInt(req.params.albumID);
+  getAlbumByID(albumID, mysqlPool)
+    .then((album) => {
+      if (album) {
+        res.status(200).json(album);
       } else {
         next();
       }
     })
     .catch((err) => {
       res.status(500).json({
-        error: "Unable to fetch business.  Please try again later."
+        error: "Unable to fetch album.  Please try again later."
       });
     });
 });
 
 /*
- * Executes a MySQL query to replace a specified business with new data.
- * Returns a Promise that resolves to true if the business specified by
- * `businessID` existed and was successfully updated or to false otherwise.
+ * Executes a MySQL query to replace a specified album with new data.
+ * Returns a Promise that resolves to true if the album specified by
+ * `albumID` existed and was successfully updated or to false otherwise.
  */
-function replaceBusinessByID(businessID, business, mysqlPool) {
+function replaceAlbumByID(albumID, album, mysqlPool) {
   return new Promise((resolve, reject) => {
-    business = validation.extractValidFields(business, businessSchema);
-    mysqlPool.query('UPDATE businesses SET ? WHERE id = ?', [ business, businessID ], function (err, result) {
+    album = validation.extractValidFields(album, albumSchema);
+    mysqlPool.query('UPDATE albums SET ? WHERE id = ?', [ album, albumID ], function (err, result) {
       if (err) {
         reject(err);
       } else {
@@ -239,18 +232,18 @@ function replaceBusinessByID(businessID, business, mysqlPool) {
 }
 
 /*
- * Route to replace data for a business.
+ * Route to replace data for a album.
  */
-router.put('/:businessID', function (req, res, next) {
+router.put('/:albumID', function (req, res, next) {
   const mysqlPool = req.app.locals.mysqlPool;
-  const businessID = parseInt(req.params.businessID);
-  if (validation.validateAgainstSchema(req.body, businessSchema)) {
-    replaceBusinessByID(businessID, req.body, mysqlPool)
+  const albumID = parseInt(req.params.albumID);
+  if (validation.validateAgainstSchema(req.body, albumSchema)) {
+    replaceAlbumByID(albumID, req.body, mysqlPool)
       .then((updateSuccessful) => {
         if (updateSuccessful) {
           res.status(200).json({
             links: {
-              business: `/businesses/${businessID}`
+              album: `/albums/${albumID}`
             }
           });
         } else {
@@ -260,24 +253,24 @@ router.put('/:businessID', function (req, res, next) {
       .catch((err) => {
         console.log(err);
         res.status(500).json({
-          error: "Unable to update specified business.  Please try again later."
+          error: "Unable to update specified album.  Please try again later."
         });
       });
   } else {
     res.status(400).json({
-      error: "Request body is not a valid business object"
+      error: "Request body is not a valid album object"
     });
   }
 });
 
 /*
- * Executes a MySQL query to delete a business specified by its ID.  Returns
- * a Promise that resolves to true if the business specified by `businessID`
+ * Executes a MySQL query to delete a album specified by its ID.  Returns
+ * a Promise that resolves to true if the album specified by `albumID`
  * existed and was successfully deleted or to false otherwise.
  */
-function deleteBusinessByID(businessID, mysqlPool) {
+function deleteAlbumByID(albumID, mysqlPool) {
   return new Promise((resolve, reject) => {
-    mysqlPool.query('DELETE FROM businesses WHERE id = ?', [ businessID ], function (err, result) {
+    mysqlPool.query('DELETE FROM albums WHERE id = ?', [ albumID ], function (err, result) {
       if (err) {
         reject(err);
       } else {
@@ -289,12 +282,12 @@ function deleteBusinessByID(businessID, mysqlPool) {
 }
 
 /*
- * Route to delete a business.
+ * Route to delete a album.
  */
-router.delete('/:businessID', function (req, res, next) {
+router.delete('/:albumID', function (req, res, next) {
   const mysqlPool = req.app.locals.mysqlPool;
-  const businessID = parseInt(req.params.businessID);
-  deleteBusinessByID(businessID, mysqlPool)
+  const albumID = parseInt(req.params.albumID);
+  deleteAlbumByID(albumID, mysqlPool)
     .then((deleteSuccessful) => {
       if (deleteSuccessful) {
         res.status(204).end();
@@ -304,22 +297,22 @@ router.delete('/:businessID', function (req, res, next) {
     })
     .catch((err) => {
       res.status(500).json({
-        error: "Unable to delete business.  Please try again later."
+        error: "Unable to delete album.  Please try again later."
       });
     });
 });
 
 /*
- * Executes a MySQL query to fetch all businesses owned by a specified user,
+ * Executes a MySQL query to fetch all albums owned by a specified user,
  * based on on the user's ID.  Returns a Promise that resolves to an array
- * containing the requested businesses.  This array could be empty if the
- * specified user does not own any businesses.  This function does not verify
+ * containing the requested albums.  This array could be empty if the
+ * specified user does not own any albums.  This function does not verify
  * that the specified user ID corresponds to a valid user.
  */
-function getBusinessesByOwnerID(userID, mysqlPool) {
+function getAlbumsByOwnerID(userID, mysqlPool) {
   return new Promise((resolve, reject) => {
     mysqlPool.query(
-      'SELECT * FROM businesses WHERE ownerid = ?',
+      'SELECT * FROM albums WHERE ownerid = ?',
       [ userID ],
       function (err, results) {
         if (err) {
@@ -333,4 +326,4 @@ function getBusinessesByOwnerID(userID, mysqlPool) {
 }
 
 exports.router = router;
-exports.getBusinessesByOwnerID = getBusinessesByOwnerID;
+exports.getAlbumsByOwnerID = getAlbumsByOwnerID;
